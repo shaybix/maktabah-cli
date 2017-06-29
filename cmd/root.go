6b7cfe7644534be298f987fcd1ee44d644b0ff64
Abdisamad Hashi <shaybix@tuta.io>
2017-06-29 23:53:36 +0100

    initial setup of the rootCmd

---
+++ cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// RootCmd is the root command representing when maktabah cli is run
+var RootCmd = &cobra.Command{
+	Use:   "maktabah",
+	Short: "Maktabah is a command line tool for Shamela Books",
+	Long: `
+A robust tool that allows you to search, download, and manage shamela books. 
+
+For more information see https://github.com/shaybix/maktabah-cli`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Println(cmd.UsageString())
+	},
+}
+
+// Execute executes the rootCmd
+func Execute() {
+
+	AddCommands()
+	if c, err := RootCmd.ExecuteC(); err != nil {
+		c.Println(c.UsageString())
+	}
+}
+
+// AddCommands adds child commands to the root command
+func AddCommands() {
+	RootCmd.AddCommand(downloadCmd)
+}
